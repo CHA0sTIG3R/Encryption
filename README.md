1e52e6063c0b049fc8a6779b6902d27db2cf3ec4
ERRATIC-TIGER <61560419+ERRATIC-TIGER@users.noreply.github.com>
2021-06-23 01:11:56 -0400

    first commit!

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ceasar Cipher Encryption
+
+**This program takes in 2 inputs:**
+1. the first is a text that is going to be encrypted
+2. second is a integer which is the shift value
+
+_This program does **NOT** encrypt numbers and special characters_
---
+++ ceasar_cipher.py
@@ -0,0 +1,18 @@
+text = input("Enter a line of text to encrypt: ")
+shift = int(input("Enter a shift value in range of 1 and 25: "))
+cipher = ""
+if shift > 25 or shift < 1:
+    print("shift value in invalid")
+else:
+    for char in text:
+        if not char.isalpha():
+            cipher += char
+            continue
+        code = ord(char) - shift
+        if char.isupper():
+            code = (ord(char) + shift - 65) % 26 + 65
+            cipher += chr(code)
+        elif char.islower():
+            code = (ord(char) + shift - 97) % 26 + 97
+            cipher += chr(code)
+print(cipher) 
